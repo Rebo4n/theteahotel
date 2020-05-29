7c0314e6f7884faf2aa7cd5c31cb1ef90d1a3ff1
unknown <bounouh.fedi9@gmail.com>
2020-05-29 02:57:47 +0000

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodAndBaverageComponent } from './food-and-baverage/food-and-baverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { OffersComponent } from './offers/offers.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accomodation', component: AccomodationComponent },
+  { path: 'celebrations', component: CelebrationsComponent },
+  { path: 'food-and-baverage', component: FoodAndBaverageComponent },
+  { path: 'lifestyle', component: LifestyleComponent },
+  { path: 'weddings', component: WeddingsComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
